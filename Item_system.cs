44db246a3b32c3912fe2997653708923e894ad41
Gabriel Sealy (1000141357) <SEALGC1@student.op.ac.nz>
2026-05-15 09:31:17 +1200

    added a working, but basic inventory system, and three more commands to aid in debugging

--- Item_system.cs
+++ Item_system.cs
@@ -3,6 +3,9 @@ using System.Net.Quic;
 using static System.Console;
 //for grabbing values from json
 using System.Text.Json;
+using System.Windows.Markup;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
 /*
 as far as im aware this just interprets the json as a list of objects (i think this is the best way to do it), so we can have a list of items in there for simplicties sake.
 i think this does mean that we will have to do some extra stuff later to get the values out of the json
@@ -10,40 +13,81 @@ i believe a json is probably the best way to do this as it's very 'modular'
 */
 string json = File.ReadAllText("items.json");
 Dictionary<string, object> Items = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
-while (true)
+//makes a new dictionary for inventory, references the 'items' dictionary, google says var is good practice in 
+var Inventory = new Dictionary<string, object>();
+WriteLine("write h, or help for a list of commands");
+bool condition = true;
+while (condition == true)
 {
     //gets the first word of input, possibility for arguments, i.e 'inspect dagger'
-    string[] input = ReadLine().Split(' ');
+    string[] input = ReadLine().ToLower().Split(' ');
     //switch for commands, dont know how we will introduce commands to player right now, maybe a 'help' command?
+    
     switch (input[0])
     {
+        //lists all commands
+        case "help":
+        case "h":
+            //someone make this better
+            WriteLine("inventory: prints contents of the inventory");
+            WriteLine("inspect: inspects item with more detail than originally shown");
+            WriteLine("help: shows a list and description of commands");
+            WriteLine("give: gives a provided item");
+            WriteLine("quit, kill, exit: closes the game");
+            break;
         case "inventory":
-            WriteLine("you have:");
+
             //i think a var would work with the below but i think one of the teachers dissaproves of var
-            foreach (KeyValuePair<string, object> item in Items)
+            if (Inventory.Count > 0)
             {
-                WriteLine(item.Key);
+                WriteLine("you have:");
+                foreach (KeyValuePair<string, object> Inv in Inventory)
+                {
+                    WriteLine(Inv.Key);
+                }
+            }
+            else
+            {
+                WriteLine("you dont have any items");
             }
             break;
         case "inspect":
-        /*
-        checks if 'input' has more than one word, if so it checks agains the key of the dictionary
-        if the second word is in the dictionary as a key it will print all the values of the key
-        otherwise, "you dont have that item"
-        do i need to use proper grammer in these comments? like full stops or capitol letters?
-        are most of these even necessary?
-        */
-            if (input.Length > 1 && Items.ContainsKey(input[1]))
+            /*
+            checks if 'input' has more than one word, if so it checks against the key of the dictionary
+            if the second word is in the dictionary as a key it will print all the values of the key
+            otherwise, "you dont have that item"
+            do i need to use proper grammar in these comments? like full stops or capitol letters?
+            are most of these even necessary?
+            */
+            if (input.Length > 1 && Inventory.ContainsKey(input[1]))
             {
                 //turns the item back into a json to get the values, then prints them. i think?
                 JsonElement item = (JsonElement)Items[input[1]];
                 foreach (JsonProperty property in item.EnumerateObject())
                     WriteLine($"{property.Name}: {property.Value}");
             }
+            
             else
             {
                 WriteLine("you dont have that item");
+            } 
+            break;
+        //give command, takes the value from the item dictionary and copies it into inventory
+        case "give":
+            if (input.Length > 1 && Items.ContainsKey(input[1]))
+            {
+                Inventory[input[1]] = Items[input[1]];
+                WriteLine($"you now have {input[1]}");
             }
+            else
+            { 
+                WriteLine("this item does not exist");
+            }
+            break;
+        case "quit":
+        case "exit":
+        case "kill":
+            condition = false;
             break;
         default:
             WriteLine("unknown command");
