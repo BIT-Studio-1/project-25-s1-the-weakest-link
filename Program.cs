7ad4d55b3415dba1cb913690d6af0e49b1a3ae02
Abby Ashton (1000140876) <ASHTWJ1@student.op.ac.nz>
2026-05-14 15:33:36 +1200

    Set up repo and project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AwesomeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
