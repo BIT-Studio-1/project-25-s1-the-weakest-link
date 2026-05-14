f5a7a8b3e10951234aceb6cffea1f33257ae59be
Gabriel Sealy (1000141357) <SEALGC1@student.op.ac.nz>
2026-05-14 15:54:46 +1200

    commit message

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace AwesomeGame
             while (option != "quit" && option != "exit")
             {
                 Console.WriteLine("what do you want to do");
-                option = Console.ReadLine().ToLower();
+                option = Console.ReadLine().Substring(0, 4).ToLower();
+                Console.WriteLine(option);
             }
         }
     }
