600c529390b55cc929944882611f25963a346208
Quinn McCormick (1000141013) <MCCOQA1@student.op.ac.nz>
2026-05-14 15:46:16 +1200

    quit option

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿namespace AwesomeGame
+﻿using System.Net.Quic;
+
+namespace AwesomeGame
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string option = "";
+
+            while (option != "quit" && option != "exit")
+            {
+                Console.WriteLine("what do you want to do");
+                option = Console.ReadLine().ToLower();
+            }
         }
     }
 }
