5a3bfb0647aa59ec8d666c01ef74760ad6fead3c
Josh Sime <SIMEJC2@student.op.ac.nz>
2026-05-21 18:41:37 +1200

    Update movementsystem.cs
    Changed movementsystem.cs to reflect a basic movement system utilizing a class and method. Currently moveEast method exists, but needs more work. Laying down foundations first before focusing on details,

--- movementsystem.cs
+++ movementsystem.cs
@@ -1,131 +1,17 @@
-//{
-//    "startRoom": {
-//        "connections": {
-//            "sideRoom": {
-//                "requirements": [],
-//                "command": "enter side room"
-//            },
-//            "hallway1": {
-//                "requirements": [],
-//                "command": "exit main entrance"
-//            },
-//            "hallway4": {
-//                "requirements": ["tablet"],
-//                "command": "exit second entrance"
-//            }
-//        },
-//        "items": ["book"]
-//    },
-//    "sideRoom": {},
-//    "hallway1": {
-//        "connections": {
-//            "startRoom": {
-//                "requirements": [],
-//                "command": "enter starting room"
-//            },
-//            "vinesRoom": {
-//                "requirements": [],
-//                "command": "enter small room"
-//            },
-//            "knifeRoom": {
-//                "requirements": [],
-//                "command": "enter open room"
-//            },
-//            "tabletRoom": {
-//                "requirements": ["tablet"],
-//                "command": "enter locked door"
-//            },
-//            "hallway2": {
-//                "requirements": ["tablet"],
-//                "command": "go south"
-//            }
-//        }
-//    },
-//    "knifeRoom": {
-//        "connections": {
-//            "hallway1": {
-//                "requirements": [],
-//                "command": "exit room"
-//            }
-//        },
-//        "items": ["book"]
-//    },
-//    "vinesRoom": {
-//        "connections": {
-//            "hallway1": {
-//                "requirements": [],
-//                "command": "exit main entrance"
-//            },
-//            "tabletRoom": {
-//                "requirements": ["cut vines"],
-//                "command": "enter main room"
-//            }
-//        }
-//    },
-//    "tabletRoom": {
-//        "connections": {
-//            "vinesRoom": {
-//                "requirements": [],
-//                "command": "enter side room"
-//            },
-//            "hallway1": {
-//                "requirements": [],
-//                "command": "exit main entrance"
-//            }
-//        },
-//        "items": ["tablet"]
-//    },
-//    "hallway2": {
-//        "connections": {
-//            "stairs": {
-//                "requirements": ["key"],
-//                "command": "enter glass door"
-//            },
-//            "hallway1": {
-//                "requirements": [],
-//                "command": "go north"
-//            },
-//            "hallway3": {
-//                "requirements": [],
-//                "command": "go west"
-//            }
-//        }
-//    },
-//    "hallway3": {
-//        "connections": {
-//            "hallway2": {
-//                "requirements": [],
-//                "command": "go east"
-//            },
-//            "hallway4": {
-//                "requirements": [],
-//                "command": "go north"
-//            }
-//        }
-//    },
-//    "hallway4": {
-//        "connections": {
-//            "startRoom": {
-//                "requirements": ["tablet"],
-//                "command": "enter starting room"
-//            },
-//            "keyRoom": {
-//                "requirements": ["tablet"],
-//                "connections": "enter locked door"
-//            },
-//            "hallway3": {
-//                "requirements": [],
-//                "command": "go south"
-//            }
-//        }
-//    },
-//    "keyRoom": {
-//        "connections": {
-//            "hallway4": {
-//                "requirements": [],
-//                "command": "exit room"
-//            }
-//        },
-//        "items": ["key"]
-//    }
-//}
+public class Movement 
+{
+   string moveEast;
+   string moveWest;
+   string moveSouth;
+   string moveNorth;
+}
+
+Movement direction = new Movement();
+
+public string MoveEast(string moveEast) 
+{
+   if (moveEast == "Go East") 
+   {
+      Console.WriteLine("You move east.");
+   }
+}
