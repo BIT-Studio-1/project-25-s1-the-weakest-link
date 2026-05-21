5c7583c4f905ea66ba2e0840a4dc270f30ea25d8
SIMEJC2 <simejc2@student.op.ac.nz>
2026-05-22 11:58:39 +1200

    Tidied up code, added breaks for switches, added a class to enable methods to work. (use using MovementSystem to utilize methods). Further variable tidy-up planned, and integration of the inventory system in the other file.

--- movementsystem.cs
+++ movementsystem.cs
@@ -1,119 +1,175 @@
-string currentRoom = "startroom";
-
-public void StartRoom(string movement) {
-   switch (movement) {
-      case "side room":
-         currentRoom = "sideroom";
-      case "main entrance":
-         currentRoom = "hallway1";
-      case "second entrance":
-         if (Inventory.Contains("tablet")) {
-         currentRoom = "hallway4";
-         }        
-   }
-}
-
-public void hallway1(string movement) {
-   switch (movement) {
-      case "starting room":
-         currentRoom = "startroom";
-      case "small room":
-         currentRoom = "vinesroom";
-      case "open room":
-         currentRoom = "kniferoom";
-      case "locked door":
-      if (Inventory.Contains("tablet")) {
-         currentRoom = "tabletroom";
-      }
-   }
-}
-
-public void kniferoom(string movement) {
-   if (movement == "hallway") {
-      currentRoom = "hallway1"
-   }
-}
-
-public void vinesroom(string movement) 
+public class MovementSystem
 {
-   switch (movement) 
-   {
-      case "main entrance":
-         currentRoom = "hallway1";
-      case "main room":
-         if (vinescut == true) 
-         {
-            currentRoom = "tabletroom";
-         }
-         else 
-         {
-            Console.WriteLine("Vines have not been cut.")
-         }         
-   }
+    string currentRoom = "startroom";
+    public void StartRoom(string movement)
+    {
+        switch (movement)
+        {
+            case "side room":
+                currentRoom = "sideroom";
+                break;
+            case "main entrance":
+                currentRoom = "hallway1";
+                break;
+            case "second entrance":
+                if (Inventory.Contains("tablet"))
+                {
+                    currentRoom = "hallway4";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have a tablet.");
+                    break;
+                }
+        }
+    }
+
+    public void hallway1(string movement)
+    {
+        switch (movement)
+        {
+            case "starting room":
+                currentRoom = "startroom";
+                break;
+            case "small room":
+                currentRoom = "vinesroom";
+                break;
+            case "open room":
+                currentRoom = "kniferoom";
+                break;
+            case "locked door":
+                if (Inventory.Contains("tablet"))
+                {
+                    currentRoom = "tabletroom";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have a tablet.");
+                    break;
+                }
+        }
+    }
+
+    public void kniferoom(string movement)
+    {
+        if (movement == "hallway")
+        {
+            currentRoom = "hallway1";
+        }
+    }
+
+    public void vinesroom(string movement)
+    {
+        switch (movement)
+        {
+            case "main entrance":
+                currentRoom = "hallway1";
+                break;
+            case "main room":
+                if (vinescut == true)
+                {
+                    currentRoom = "tabletroom";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Vines have not been cut.");
+                    break;
+                }
+        }
+    }
+
+    public void tabletroom(string movement)
+    {
+        switch (movement)
+        {
+            case "side room":
+                currentRoom = "vinesroom";
+                break;
+            case "main entrance":
+                currentRoom = "hallway1";
+                break;
+        }
+    }
+
+    public void hallway2(string movement)
+    {
+        switch (movement)
+        {
+            case "glass door":
+                if (Inventory.Contains("key"))
+                {
+                    currentRoom = "stairs";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have a key.");
+                    break;
+                }
+            case "north":
+                currentRoom = "hallway1";
+                break;
+            case "west":
+                currentRoom = "hallway3";
+                break;
+        }
+    }
+
+    public void hallway3(string movement)
+    {
+        switch (movement)
+        {
+            case "east":
+                currentRoom = "hallway2";
+                break;
+            case "north":
+                currentRoom = "hallway4";
+                break;
+        }
+    }
+
+    public void hallway4(string movement)
+    {
+        switch (movement)
+        {
+            case "starting room":
+                if (Inventory.Contains("tablet"))
+                {
+                    currentRoom = "startroom";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have a tablet.");
+                }
+            case "locked door":
+                if (Inventory.Contains("tablet"))
+                {
+                    currentRoom = "keyroom";
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("You do not have a tablet.");
+                    break;
+                }
+            case "south":
+                currentRoom = "hallway3";
+                break;
+        }
+    }
+
+    public void keyroom(string movement)
+    {
+        switch (movement)
+        {
+            case "hallway":
+                currentRoom = "hallway4";
+                break;
+        }
+    }
 }
 
-public void tabletroom(string movement) 
-{
-   switch (movement) 
-   {
-      case "side room":
-      currentRoom = "vinesroom";
-      case "main entrance":
-      currentRoom = "hallway1";
-   }
-}
-
-public void hallway2(string movement)
-{
-   switch (movement)
-   {
-      case "glass door":
-      if (Inventory.Contains("key"))
-      {
-         currentRoom = "stairs";
-      }
-      case "north":
-      currentRoom = "hallway1";
-      case "west":
-      currentRoom = "hallway3";
-   }
-}
-
-public void hallway3(string movement) 
-{
-   switch (movement)
-   {
-      case "east":
-      currentRoom = "hallway2";
-      case "north":
-      currentRoom = "hallway4";
-   }
-}
-
-public void hallway4(string movement) 
-{
-   switch (movement) 
-   {
-      case "starting room":
-      if (Inventory.Contains("tablet"))
-      {
-         currentRoom = "startroom";
-      }
-      case "locked door":
-      if (Inventory.Contains("tablet"))
-      {
-         currentRoom = "keyroom";
-      }
-      case "south":
-      currentRoom = "hallway3";
-   }
-}
-
-public void keyroom(string movement)
-{
-   switch (movement)
-   {
-      case "hallway":
-      currentRoom = "hallway4";
-   }
-}
