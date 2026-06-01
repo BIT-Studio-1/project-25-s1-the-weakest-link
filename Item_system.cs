87ac6cfd22e7e4c8526c1de9d1f623854b6d85d9
SIMEJC2 <simejc2@student.op.ac.nz>
2026-06-02 11:28:56 +1200

    EndGame method implemented, with game ending.

--- Item_system.cs
+++ Item_system.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using AwesomeGame;
@@ -63,6 +65,12 @@ internal static class Game
         bool condition = true, secretsEnabled = false;
         while (condition == true)
         {
+            if (MovementSystem.currentRoom == "staircase")
+            {
+                EndGame();
+                break;
+            }
+
             WriteLine("===============================================");
             var roomtemp = (JsonElement)Rooms[MovementSystem.currentRoom];
             int room_actions = roomtemp.GetProperty("actions").GetInt32();
@@ -398,5 +406,22 @@ internal static class Game
                     break;
             }
         }
+
+        Console.ReadLine();
+    }
+    public static void EndGame()
+    {
+        scrolltext("You carefully unlock the glass door and hesitantly push it open. Could this finally be the escape from this prison you find yourself in?", 30);
+        scrolltext("You walk inside, hanging close to the wall so as to maintain your sense of direction. Your hand connects with a slender metal bar, as a sudden drop appears before you.\r\n", 30);
+        scrolltext("You reach a foot down the cliff, clinging tight to the bar. Your body is bound tight with fear, your foot slowly descending down the edge. Suddenly, your foot finds ground, as you realise a stairwell has appeared before you.\r\n", 30);
+        scrolltext("You slowly tread down the stairs, foot by foot, step by step. As you descend, you realise with a shock that your vision is returning! Your senses are overwhelmed by a blinding light, radiating from a closed door.\r\n", 30);
+        scrolltext("Psyching yourself for danger, you open the door...........\r\n", 75);
+        scrolltext("\"Hey, the building closed to students four hours ago, it's cleaners only now.\"\r\n", 30);
+        scrolltext("You are in the ground floor of the Otago Polytechnic's D block, and you are staring face to face with the janitor.\r\n", 30);
+        scrolltext("\"It's 4am, go home.\"\r\n", 30);
+        scrolltext("THE NEXT DAY.....\r\n", 75);
+        scrolltext("You wake up in your home at 2pm, still exhausted from last night's confusion. You yawn, then get out of bed.", 30);
+        scrolltext("You go to check your mailbox and see a letter addressed to you with the polytech's logo. You open it up, and read the contents...\r\n", 30);
+        PropertyDamage.writebill();
     }
 }
